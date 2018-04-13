f76422525a82241abc175896f6265a45480d8597
bluew <bluewww@users.noreply.github.com>
2018-04-14 00:02:35 +0200

    Add hello world image

--- snake.c
+++ snake.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #define SCREEN_WIDTH 640
@@ -9,35 +10,45 @@ int main(int argc, char *argv[argc + 1])
 {
     SDL_Window *window = NULL;
     SDL_Surface *screen_surface = NULL;
+    SDL_Surface *splash_image = NULL;
 
     int status = EXIT_SUCCESS;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-	printf("SDL could not initialize, %s\n", SDL_GetError());
-	return EXIT_FAILURE;
+        printf("SDL could not initialize, %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
     window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_UNDEFINED,
-			      SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-			      SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+                              SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
-	printf("Window could not be created, %s\n", SDL_GetError());
-	return EXIT_FAILURE;
+        printf("Window could not be created, %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     }
 
     screen_surface = SDL_GetWindowSurface(window);
     if (!screen_surface) {
-	printf("Screen surface could not be created, %s\n", SDL_GetError());
-	status = EXIT_FAILURE;
-	goto CLEANUP;
+        printf("Screen surface could not be created, %s\n", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto cleanup0;
     }
 
-    SDL_FillRect(screen_surface, NULL,
-		 SDL_MapRGB(screen_surface->format, 0xff, 0xff, 0xff));
+    splash_image = SDL_LoadBMP("hello_world.bmp");
+    if (!splash_image) {
+        printf("Couldn't load image, %s\n", SDL_GetError());
+        status = EXIT_FAILURE;
+        goto cleanup1;
+    }
+
+    SDL_Delay(2000);
+    SDL_BlitSurface(splash_image, NULL, screen_surface, NULL);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(2000);
 
-CLEANUP:
+cleanup1:
+    SDL_FreeSurface(splash_image);
+cleanup0:
     SDL_DestroyWindow(window);
     SDL_Quit();
 
