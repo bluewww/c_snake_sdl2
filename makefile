a63853bb4d6a4524f47b8cda5e028778adf1b32d
bluew <bluewww@users.noreply.github.com>
2018-04-16 23:50:34 +0200

    Add world and snake skeletons

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 compile1: snake.c
-	gcc-7 --std=c11 -Wall -o snake snake.c -lSDL2 -I.
+	gcc-7 --std=c11 -ggdb -O0 -Wall -o snake snake.c -lSDL2 -I.
--- snake.c
+++ snake.c
@@ -1,16 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+
 #include <SDL2/SDL.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-SDL_Surface *load_surface(char path[1]);
+struct world {
+    uint32_t tile_size_x;  // 16;
+    uint32_t tile_size_y;  // 16;
+    uint32_t world_size_x; // 40;
+    uint32_t world_size_y; // 30;
+    SDL_Surface *black_surface;
+    SDL_Surface *white_surface;
+};
+
+struct snake_part {
+    struct snake_part *prev;
+    uint32_t x;
+    uint32_t y;
+};
+
+SDL_Surface *load_surface(const char path[1]);
 SDL_Surface *create_color_surface(Uint32 width, Uint32 height, Uint32 r,
                                   Uint32 g, Uint32 b);
+// TODO: snake queue
+// TODO: food spawn logic
+// TODO: struct world
+void render_world(struct world *, struct snake_part *, SDL_Surface *);
+void update_all();
 
-SDL_Surface *load_surface(char path[1])
+void render_world(struct world *world, struct snake_part *snake,
+                  SDL_Surface *surface)
+{
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = (int)world->tile_size_x;
+    rect.h = (int)world->tile_size_y;
+
+    /* Render world */
+    for (uint32_t x = 0; x < world->world_size_x; x++) {
+        for (uint32_t y = 0; y < world->world_size_y; y++) {
+            rect.x = x * world->world_size_x;
+            rect.y = y * world->world_size_y;
+            SDL_BlitSurface(world->white_surface, NULL, surface, &rect);
+        }
+    }
+    //TODO: render snake
+}
+SDL_Surface *load_surface(const char path[1])
 {
     SDL_Surface *surface = SDL_LoadBMP(path);
     if (!surface) {
@@ -41,7 +82,7 @@ int main(int argc, char *argv[argc + 1])
     SDL_Window *window = NULL;
     SDL_Surface *screen_surface = NULL;
     SDL_Surface *splash_image = NULL;
-    //    SDL_Surface *white_surface = NULL;
+    SDL_Surface *white_surface = NULL;
     SDL_Surface *black_surface = NULL;
     SDL_Rect tile;
     SDL_Event event;
@@ -49,6 +90,7 @@ int main(int argc, char *argv[argc + 1])
 
     int status = EXIT_SUCCESS;
 
+    /* Init SDL stuff */
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize, %s\n", SDL_GetError());
         return EXIT_FAILURE;
@@ -75,10 +117,25 @@ int main(int argc, char *argv[argc + 1])
         goto cleanup1;
     }
 
-    black_surface = create_color_surface(100, 100, 255, 0, 0);
+    /* Init world stuff */
+    // TODO: finish the snake
+    struct snake_part snake;
+    struct world world;
+    world.tile_size_x = 16;
+    world.tile_size_y = 16;
+    world.world_size_x = 40;
+    world.world_size_y = 30;
+
+    world.black_surface = create_color_surface(100, 100, 0, 0, 0);
     if (!black_surface) {
         status = EXIT_FAILURE;
         goto cleanup2;
+    }
+
+    world.white_surface = create_color_surface(100, 100, 255, 255, 255);
+    if (!white_surface) {
+        status = EXIT_FAILURE;
+        goto cleanup3;
     }
 
     while (!quit) {
@@ -95,17 +152,22 @@ int main(int argc, char *argv[argc + 1])
         }
         tile.x = 10;
         tile.y = 10;
-        SDL_BlitSurface(splash_image, NULL, screen_surface, NULL);
-        //        SDL_BlitSurface(white_surface, NULL, screen_surface, NULL);
-        SDL_BlitSurface(black_surface, NULL, screen_surface, &tile);
+        /* SDL_BlitSurface(splash_image, NULL, screen_surface, NULL); */
+        /* SDL_BlitSurface(world.white_surface, NULL, screen_surface, NULL); */
+        /* SDL_BlitSurface(world.black_surface, NULL, screen_surface, &tile); */
+	render_world(&world, &snake, screen_surface);
         SDL_UpdateWindowSurface(window);
     }
-// cleanup3:
-//    SDL_FreeSurface(white_surface);
+
+cleanup3:
+    SDL_FreeSurface(world.white_surface);
+    world.white_surface = NULL;
 cleanup2:
-    SDL_FreeSurface(black_surface);
+    SDL_FreeSurface(world.black_surface);
+    world.black_surface = NULL;
 cleanup1:
     SDL_FreeSurface(splash_image);
+    splash_image = NULL;
 cleanup0:
     SDL_DestroyWindow(window);
     SDL_Quit();
